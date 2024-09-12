a96813ad84d4abdce3817988120481b97f3d8daf
Raíque Souza <00001137247976SP@al.educacao.sp.gov.br>
2024-09-11 21:09:31 -0300

    Importando os tickers, e começando a manipulação das planilhas

--- Index.py
+++ Index.py
@@ -3,4 +3,7 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import mplcyberpunk 
 
-tickets = []
+tickers = ["^BVSP", "^GSPC", "^BRL=X"]
+dados_mercado = yf.download(tickets, period = '6mo')
+dados_mercado = dados_mercado["Adj close"]
+dados_mercado.head(50)
