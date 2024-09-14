cf9f33c0ae9bbd0b9996ce01cf7eff3da962be3f
Rqoguridochima <00001137247976SP@al.educacao.sp.gov.br>
2024-09-14 17:39:41 -0300

    concertando e adicionando o codigo

--- Index.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import yfinance as yf
-import matplotlib.pyplot as plt
-import mplcyberpunk 
-
-tickers = ["^BVSP", "^GSPC", "^BRL=X"]
-dados_mercado = yf.download(tickets, period = '6mo')
-dados_mercado = dados_mercado["Adj close"]
-dados_mercado.head(50)
